fdd08e000e88bad41977a60756210d1e8963025b
Roman Kozin <romanko95.rk@gmail.com>
2021-05-03 00:12:29 +0300

    WIP - Updated trait - Updated meta config

--- src/Traits/HasMeta.php
+++ src/Traits/HasMeta.php
@@ -4,12 +4,21 @@ declare(strict_types=1);
 
 namespace KielD01\Traits;
 
-use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
+/**
+ * Trait HasMeta
+ * @package KielD01\Traits
+ * @property MorphOne|MorphMany|MorphToMany meta
+ */
 trait HasMeta
 {
-	public function meta(): MorphTo
+	public function meta()
 	{
-		return $this->morphOne(static::class, 'parent');
+		$metaMorph = $this->__get('metaMorphType') ?? config('laravel-meta.defaultMorph', 'morphOne');
+
+		return $this->{$metaMorph}(static::class, 'parent');
 	}
 }
--- src/configs/laravel-meta.php
+++ src/configs/laravel-meta.php
@@ -8,4 +8,5 @@ return [
 			'name' => null,
 		],
 	],
+	'defaultMorph' => null,
 ];
